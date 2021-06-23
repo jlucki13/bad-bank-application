d162ff3435454c645f487e88594151bc1f9241b6
jlucki13 <69769168+jlucki13@users.noreply.github.com>
2021-06-22 20:30:44 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="black"
+      header="BadBank Landing Module"
+      title="We Are... Bad Bank (BB)"
+      text="Welcome to the Bad Bank (BB) website, you can create a new account, deposit or withdrawal money, and see your latest account info"
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
